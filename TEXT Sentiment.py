fc8a99ee0c9a0765234162b1523ec44f21fceeda
Nagaratna29 <naiknagaratna04@gmail.com>
2026-07-15 15:14:17 +0500

    Update TEXT Sentiment.py

--- TEXT Sentiment.py
+++ TEXT Sentiment.py
@@ -1,4 +1,4 @@
- import pandas as pd
+import pandas as pd
 import numpy as np
 import nltk
 nltk.download('punkt')
@@ -7,6 +7,7 @@ nltk.download('stopwords')
 import re
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
+from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
@@ -23,10 +24,11 @@ df.duplicated().sum()
 df=df.drop_duplicates()
 df.shape
 df.isnull().sum()
-df['Review'].astype(str)
-df['Review'].str.lower()
+df["Sentiment"].value_counts()
+df['Review'] = df['Review'].astype(str)
+df['Review'] = df['Review'].str.lower()
 df['Review'].head()
-df["Review"].apply(lambda x: re.sub(r'[^a-zA-Z\s]', '', x))
+df["Review"] = df['Review'].apply(lambda x: re.sub(r'[^a-zA-Z\s]', '', x))
 df['Review'].head()
 df['Review'] = df['Review'].apply(word_tokenize)
 stop_words = set(stopwords.words('english'))
@@ -38,9 +40,14 @@ df['Review'] = df['Review'].apply(lambda x: " ".join(x))
 df['Review'].head()
 x=df['Review']
 y=df['Sentiment']
-x
+tfidf = TfidVectorizer()
+x = tfidf.fit_transform(df['Review'])
+print(x.shape)
+le = LabelEncoder()
+y = le.fit_transform(df['Sentiment'])
+print(y)
 x_train, x_test, y_train, y_test=train_test_split(x, y, test_size=0.2, random_state=42)
-LogisticRegression(max_iter=1000,  class_weight='balanced',)
+LogisticRegression(max_iter=1000, class_weight='balanced',)
 model.fit(x_train, y_train)
 y_pred=model.predict(x_test)
 accuracy = accuracy_score(y_test, y_pred)
